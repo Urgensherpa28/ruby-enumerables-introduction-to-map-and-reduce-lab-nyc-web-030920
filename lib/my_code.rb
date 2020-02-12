0bbedd309b6dc2a3419ef1431ff7800413966ce9
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-12 19:22:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,66 @@
-# My Code here....
+# My Code here...
+def map_to_negativize(source_array)
+  new_array = [] 
+  source_array.each do |nums| 
+    new_array << nums * -1
+  end
+  new_array
+end
+
+
+def map_to_no_change(source_array)
+   source_array
+end
+
+
+def map_to_double(source_array)
+  new_array = []
+  source_array.each do |num|
+    new_array << num * 2 
+  end
+  new_array
+end
+
+
+def map_to_square(source_array)
+  new_array = [] 
+  source_array.each do |num|
+    new_array << num ** 2 
+  end 
+  new_array
+end
+
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  counter = 0 
+  while counter < source_array.length do 
+    total += source_array[counter]
+    counter += 1 
+  end
+  total 
+end
+
+
+def reduce_to_all_true(source_array)
+  counter = 0 
+  while counter < source_array.length do
+    if counter == false 
+      return false 
+      counter += 1 
+    end 
+  return true  
+ end
+end
+
+
+def reduce_to_any_true(source_array)
+  counter = 0 
+  while counter < source_array.length do 
+    if counter == true  
+      return true 
+      counter += 1 
+    end
+  return false 
+  end
+end 
