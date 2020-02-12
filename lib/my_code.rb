dd05e7d5a83be4a9161ec3ec01de5e032a899354
Urgen Sherpa <urgen.nyc@gmail.com>
2020-02-12 19:51:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,12 +45,12 @@ end
 def reduce_to_all_true(source_array)
   counter = 0 
   while counter < source_array.size do
-    if source_array[counter] == true  
-      return true
+    if source_array[counter] == false   
+      return false 
     end 
     counter += 1 
   end
-  return false       
+  return true        
 end
 
 
